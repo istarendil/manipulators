570010011e905e05f06012bef05c2fda35fae15c
istarien <alberto.iztatik@gmail.com>
2022-10-11 14:27:02 -0500

    deleting duplicated file in elbow/drivers

--- elbow/drivers/ik_demo.py
+++ elbow/drivers/ik_demo.py
@@ -1,39 +1,55 @@
 import pybullet as pb
-from solver.solver import Solver
 import pybullet_data
-import numpy as np
+from solver.solver import Solver
+from numpy import pi
 
-# Make an instance of a physic client
-phy_client = pb.connect(pb.GUI)
+class Elbow(Solver):
+    def __init__(self, robot, d1, a2, d4, d6):
+        super().__init__(d1, a2, d4, d6)
+        self._robot = robot
 
-# Simulation parameters
-pb.setGravity(0, 0, -9.81)
-pb.setRealTimeSimulation(0)
+    def set_joints(self, position, orientation):
+        q = self.solve_ik(position, orientation)
+        pb.setJointMotorControlArray(self._robot, 
+                                 range(pb.getNumJoints(self._robot)),
+                                 pb.POSITION_CONTROL,
+                                 targetPositions = q)
 
-# Objects
-pb.setAdditionalSearchPath(pybullet_data.getDataPath())
-ground = pb.loadURDF('plane.urdf')
-table = pb.loadURDF('table/table.urdf') 
-cube = pb.loadURDF('cube.urdf', basePosition = [0.15, 0.0, 0.775], globalScaling = 0.3)
-elbow = pb.loadURDF('urdf/elbow.urdf.xml', basePosition=[-0.4, 0.0, 0.625], useFixedBase=1)
-
-# Inverse kinematics
-o = (0.4, -0.15, 0.3)
-rpy = (0.0, np.pi/2, np.pi/4)
-my_solver = Solver(0.17, 0.25, 0.23, 0.16)
-q = my_solver.solve_ik(o, rpy, -1)
-print('q: ', q)
+    def move(self):
+        input('Press Enter to continue...')
+        for _ in range(1000):
+            pb.stepSimulation()
 
 
-# Execute simulation
-pb.setJointMotorControlArray(elbow, range(6), pb.POSITION_CONTROL, targetPositions = q)
 
-input('Press any key to continue')
-for _ in range(4000):
-    pb.stepSimulation()
+if __name__ == '__main__':
+    # Make an instance of a physic client
+    client = pb.connect(pb.GUI)
 
-# Disconnect the physic client
-input('Press any key to stop...')
+    # Simulation parameters
+    pb.setGravity(0, 0, -9.81)
+    pb.setRealTimeSimulation(0)
+
+    #Environment 
+    pb.setAdditionalSearchPath(pybullet_data.getDataPath())
+    floor = pb.loadURDF('plane.urdf')
+    table = pb.loadURDF('table/table.urdf')
+    cube = pb.loadURDF('cube.urdf', basePosition = [0.15, 0.0, 0.775], globalScaling = 0.3)
+    robot_urdf = pb.loadURDF('../urdf/elbow.urdf.xml', 
+                        basePosition = [-0.4, 0.0, 0.625],
+                        useFixedBase = 1)
+    
+    # Instanciating a Scara robot
+    elbow = Elbow(robot_urdf, 0.17, 0.25, 0.23, 0.16)
+
+    # Movement sequence (xyz, rpy)
+    poses =  (((0.4, -0.15, 0.3), (-pi/4, 3*pi/4, 0.0)),
+             ((0.4, 0.15, 0.3), (pi/4, 3*pi/4, 0.0)))
+
+    for pose in poses:
+        elbow.set_joints(pose[0], pose[1])
+        elbow.move()
+
+# End program
+input('Press Enter to stop...')
 pb.disconnect()
-
-
